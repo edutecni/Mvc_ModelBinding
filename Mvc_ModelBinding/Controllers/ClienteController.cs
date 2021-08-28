1987ba1f329a356a4fc32522b686b92bf7e79729
Eduardo Alves Canuto <edutecni@gmail.com>
2021-08-28 17:31:21 -0300

    ok3

---
+++ Mvc_ModelBinding/Controllers/ClienteController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mvc_ModelBinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mvc_ModelBinding.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Cliente cliente)
+        {
+            if (cliente?.ClienteId == 0 || cliente?.Nome == null || cliente?.Email == null)
+            {
+                ViewBag.Erro = "Dados do cliente Inválido!";
+                return View();
+            }
+            else
+            {
+                return View("ExibirDados", cliente);
+            }
+        }
+    }
+}
